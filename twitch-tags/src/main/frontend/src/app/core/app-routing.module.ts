08e272edb84b516977f175e28d259fb189c30fb5
Gwenaelle Rispal <gwenaelle.rispal@gmail.com>
2018-12-13 16:03:15 +0100

    fix tag update on popular component

--- twitch-tags/src/main/frontend/src/app/core/app-routing.module.ts
+++ twitch-tags/src/main/frontend/src/app/core/app-routing.module.ts
@@ -5,8 +5,6 @@ import {ChannelProfileComponent} from './channel-profile/channel-profile.compone
 import {HomeComponent} from "./home/home.component";
 import {ErrorComponent} from "./error/error.component";
 
-import {ChannelResolver} from "../services/channel-resolver.service";
-
 const routes: Routes = [
   {path: '', redirectTo: 'app', pathMatch: 'full'},
   {path: 'app', component: HomeComponent},
